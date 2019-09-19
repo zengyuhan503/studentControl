3c8c62cdc62700abc78bff7c4b099e1f2f471ad4
ZENGYUHAN <13458685625@163.com>
2019-09-19 18:16:55 +0800

    home -lfetnav

--- baoge/src/App.vue
+++ baoge/src/App.vue
@@ -20,7 +20,7 @@
         >
           <el-submenu index="1">
             <template slot="title">
-              <i class="el-icon-document"></i>
+              <i class="el-icon-user-solid"></i>
               <span>账户管理</span>
             </template>
             <el-menu-item-group>
@@ -32,16 +32,26 @@
           </el-submenu>
           <el-submenu index="2">
             <template slot="title">
-              <i class="el-icon-goods"></i>
-              <span>应用商店管理</span>
+              <i class="el-icon-document"></i>
+              <span>统计信息</span>
             </template>
+            <el-menu-item-group>
+              <el-menu-item index="/studentinfo">信息列表</el-menu-item>
+            </el-menu-item-group>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title">
-              <i class="el-icon-picture"></i>
-              <span>视频管理</span>
+              <i class="el-icon-message-solid"></i>
+              <span>报警设置</span>
             </template>
+            <el-menu-item-group>
+              <el-menu-item index="/studentinfo">报警列表</el-menu-item>
+            </el-menu-item-group>
           </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-s-platform"></i>
+            <span slot="title">充值设置</span>
+          </el-menu-item>
         </el-menu>
       </el-col>
       <el-col :span="21" class="mainCont">
